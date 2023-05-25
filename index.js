63c418f117d911838cb95ac1cc569b621a760d60
Will Lowenkamp <lowenkamp97@gmail.com>
2023-05-25 11:38:24 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+ function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+ }
+const leastFavoriteCustomer = "mike"
+ function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "will"
+ }
